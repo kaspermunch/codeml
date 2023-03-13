fcb54be7ea1b04bfd83e9f1a0ff8bbfeec750acc
kaspermunch <kaspermunch@birc.au.dk>
2023-03-13 09:48:37 +0100

    Now requiring at least 4 sequences and only indels in multiples of three

--- scripts/assembleCDS.py
+++ scripts/assembleCDS.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import gzip
+import copy # for testsing / debugging ... remove later
 import pandas as pd
 from collections import defaultdict
 from contextlib import redirect_stdout
@@ -46,8 +47,21 @@ def write_fasta(seqs, output_file):
         for name in sorted(seqs.keys(), key=lambda x: x != 'hg38'):
             print(f">{name}\n{seqs[name]}")            
 
+def print_aln(gene_name, seqs, f):
+
+    with redirect_stdout(f):
+
+        lengths = [len(x) for x in seqs.values()]
+        assert(all(lengths[0] == l for l in lengths))
+        seq_length = lengths[0]
+
+        for name in sorted(seqs.keys(), key=lambda x: x != 'hg38'):
+            print(f"{gene_name:<10} {name:<7} {seqs[name]}")  
+        print()
+        print()
+
 # command line arguments
-_, fasta_file, id_table_file, tree_file, aln_stat_file, output_dir = sys.argv
+_, fasta_file, id_table_file, tree_file, aln_stat_file, output_dir, discarded_aln_file = sys.argv
 
 # read id table
 id_table = pd.read_csv(id_table_file, sep='\t').drop_duplicates('name2')
@@ -70,7 +84,7 @@ tree.unroot()
 species_included = {}
 
 # read fasta from gziped (compressed file)
-with gzip.open(fasta_file, 'rt') as f:
+with gzip.open(fasta_file, 'rt') as f, open(discarded_aln_file, 'w') as f_discarded:
 
     # keep track of what the last ucsc id we have seen is
     prev_ucsc_id = None
@@ -99,7 +113,6 @@ with gzip.open(fasta_file, 'rt') as f:
                 skipped += 1
             else: 
                 # if it was possible, we go on
-                print(gene_name)
 
                 # make a dictionary with the concatenated exons (CDS) for each gene
                 cds_alignment = {}
@@ -107,46 +120,65 @@ with gzip.open(fasta_file, 'rt') as f:
                     if species not in excluded_species:
                         cds_alignment[species] = ''.join(exons[species]).upper()
 
+                ##################                            
+                # For testing... keep original
+                test_cds_alignment = copy.deepcopy(cds_alignment)
+                ##################                            
+
                 # # regular expression for selecting only those with an aligned start and top codon and no inframe stop codons:
                 regex = re.compile(r'ATG(?:(?!TAA|TAG|TGA)...)*(?:TAA|TAG|TGA)$')
-
-                # # regular expression for truncating at inframe stop codons:
-                # regex = re.compile(r'(?:(?!TAA|TAG|TGA)...)*(?:TAA|TAG|TGA)')
+                # This version also tests that all indels are multiples of inframe triplets (discards a lot of candidate genes...)
+                # regex = re.compile(r'ATG(?:(?!TAA|TAG|TGA|[TAGC][TAGC]-|-[TAGC][TAGC]|[TAGC]-[TAGC]|[TAGC]--|--[TAGC]|-[TAGC]-)...)*(?:TAA|TAG|TGA)$')
 
                 # use the regular expression on each sequence
                 for species in list(cds_alignment.keys()):
-                    match = regex.match(cds_alignment[species])
+                    orf_ok = regex.match(cds_alignment[species])
 
-                    if match:
-                        # remove the stop codon found
-                        start, end = match.span()
+                    # test if all indels are multiples of triplets, not necesarrily inframe
+                    no_frame_shifting_indels = not any(x % 3 for x in map(len, re.findall(r'-+', cds_alignment[species])))
+
+                    if orf_ok and no_frame_shifting_indels:
+                        # remove the stop codon found (becuase codeml does not want it in there)
+                        start, end = orf_ok.span()
                         cds_alignment[species] = cds_alignment[species][:end-3] + '?' * (len(cds_alignment[species]) - end + 3)
                     else:
                         # delete the species and their sequences if the regular expression does not match
-                        del cds_alignment[species]                        
+                        del cds_alignment[species]                                             
 
                 # keep the alignmnets with human and at least two other species
-                if is_coding and 'hg38' in cds_alignment and len(cds_alignment) >= 2:
+                if is_coding and 'hg38' in cds_alignment and len(cds_alignment) >= 4:
                     
-                    # record which species are in the alignment
-                    species_included[gene_name] = list(cds_alignment.keys())
+                    # # record which species are in the alignment
+                    # species_included[gene_name] = list(cds_alignment.keys())
 
-                    # write phylip file
-                    output_path = os.path.join(output_dir, chrom, gene_name, gene_name + '.phylib')
-                    write_phylip(cds_alignment, output_path)                  
+                    # # write phylip file
+                    # output_path = os.path.join(output_dir, chrom, gene_name, gene_name + '.phylib')
+                    # write_phylip(cds_alignment, output_path)                  
 
-                    # write fasta file (in case you need it)
-                    output_path = os.path.join(output_dir, chrom, gene_name, gene_name + '.fa')
-                    write_fasta(cds_alignment, output_path)                  
+                    # # write fasta file (in case you need it)
+                    # output_path = os.path.join(output_dir, chrom, gene_name, gene_name + '.fa')
+                    # write_fasta(cds_alignment, output_path)                  
 
-                    # remove the species from the tree that were removed from the alignment
-                    alignment_tree = tree.copy("newick")
-                    alignment_tree.prune(list(cds_alignment.keys()))
+                    # # remove the species from the tree that were removed from the alignment
+                    # alignment_tree = tree.copy("newick")
+                    # alignment_tree.prune(list(cds_alignment.keys()))
 
-                    # write the tree for the alignment
-                    output_path = os.path.join(output_dir, chrom, gene_name, gene_name + '.nw')
-                    alignment_tree.write(format=1, outfile=output_path)
+                    # # write the tree for the alignment
+                    # output_path = os.path.join(output_dir, chrom, gene_name, gene_name + '.nw')
+                    # alignment_tree.write(format=1, outfile=output_path)                    
+
+                    # print(gene_name)
+
+                    pass
+
                 else:
+                    ###################
+                    print('SKIPPED', chrom, gene_name, file=f_discarded)
+                    print_aln(gene_name, test_cds_alignment, f_discarded)
+                    ##################                            
+
+                    # grep SKIPPED steps/discarded_genes.txt | cut -f 3 -d ' ' | xargs -I GENE find steps/ -name 'GENE.*' -exec rm -f {} \;
+
                     skipped += 1
 
                 # empty the exon dictionary 
